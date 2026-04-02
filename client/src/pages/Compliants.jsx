ce85d1dd51481a4a200c6d74a299621a516d89c9
Alem-Mamo <119974698+Alem-Mamo@users.noreply.github.com>
2026-04-02 18:23:13 +0300

    internationalize video and audio recording labels and descriptions in complaint form

--- client/src/pages/Compliants.jsx
+++ client/src/pages/Compliants.jsx
@@ -450,7 +450,8 @@ function Compliants() {
                         <Upload photo={formData.photo} setFormData={setFormData} />
                     </div>
                     <div>
-                        <p className='font-roboto text-xs text-gray-500 mb-3'>You can also record video evidence for your complaint.</p>
+                        <label className='block font-roboto font-medium text-sm mb-1 text-gray-700'>{t.complaint_form.fields.video_recording.label[language]}</label>
+                        <p className='font-roboto text-xs text-gray-500 mb-3'>{t.complaint_form.fields.video_recording.description[language]}</p>
                         <MediaRecorderComponent 
                             type="video" 
                             initialMedia={formData.video} 
@@ -458,7 +459,8 @@ function Compliants() {
                         />
                     </div>
                     <div>
-                        <p className='font-roboto text-xs text-gray-500 mb-3'>You can also record audio evidence for your complaint.</p>
+                        <label className='block font-roboto font-medium text-sm mb-1 text-gray-700'>{t.complaint_form.fields.audio_recording.label[language]}</label>
+                        <p className='font-roboto text-xs text-gray-500 mb-3'>{t.complaint_form.fields.audio_recording.description[language]}</p>
                         <MediaRecorderComponent 
                             type="audio" 
                             initialMedia={formData.audio} 
